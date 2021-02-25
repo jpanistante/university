26316a8d0829f8ec5bd6a131d8506c8c637cdba9
jpanistante <jpanistante@academic.rrc.ca>
2021-02-24 21:40:52 -0600

    Addded Controllers for Students and Departments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'departments/index'
+  get 'departments/show'
+  get 'students/index'
+  get 'students/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
