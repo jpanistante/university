9a307e1a52ae0cf8e262ef02e44b54a3c3829bf1
jpanistante <jpanistante@academic.rrc.ca>
2021-02-24 21:46:11 -0600

    Changed routes.rb to use index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'departments/index'
-  get 'departments/show'
-  get 'students/index'
-  get 'students/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :students, only: %i[index show]
+  resources :departments, only: %i[index show]
 end
