5d90117bb4fde70024c78ba64a49eb29f6e71172
jpanistante <jpanistante@academic.rrc.ca>
2021-02-24 23:56:25 -0600

    Load all departments

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,7 +1,10 @@
 class DepartmentsController < ApplicationController
   def index
+    # Using the department model, load all departments from the db
+    @departments = Department.all
+
+    # This will end up in the index view (html.erb)
   end
 
-  def show
-  end
+  def show; end
 end
