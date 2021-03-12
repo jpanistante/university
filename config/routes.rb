35164cf2569595335fd64e23668ef818bbb8f5d6
jpanistante <jpanistante@academic.rrc.ca>
2021-03-12 07:31:30 -0600

    Created permalink routes for About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages, except: [:show]
+  get "/pages/:permalink" => "pages#permalink", as: "permalink"
   resources :students, only: %i[index show]
   resources :departments, only: %i[index show]
   root to: "home#index"
