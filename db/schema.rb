29c445835133af16a505f3ef66603e2952486d4a
jpanistante <jpanistante@academic.rrc.ca>
2021-03-12 07:13:47 -0600

    Removed scaffolding css

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_200646) do
+ActiveRecord::Schema.define(version: 2021_03_12_130021) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require "faker"
 # Faker::Name.unique.clear
 # Faker::Internet.email(name: Faker::Name).clear
 
+Page.delete_all
 Student.delete_all
 Department.delete_all
 
@@ -31,6 +32,15 @@ departments.each do |row|
                       }])
   end
 end
+
+Page.create(title:     "About",
+            content:   "Students (generated through Faker)
+            Every student must have a name, username and email. No validation for gender for real-life purposes. Username and email must be unique as no two students can have the same credentials. This model will reference Departments. When clicked individually, it will “show” the username and email of that specific student.
+
+            Departments (generated through CSV file)
+            A department has a name. There are 40 departments (fixed). This model will be referenced by Students. If a new department is added, it must have be unique and have a name. When clicked individually it will “show” the students under that department.
+            ",
+            permalink: "About")
 
 puts "Created #{Department.count} Departments."
 puts "Created #{Student.count} Students."
